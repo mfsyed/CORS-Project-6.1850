4bb8d9d48bb10a8d8328402b0fb1dbfc5ecf8335
Malobika Syed <42706046+mfsyed@users.noreply.github.com>
2022-12-01 17:57:39 -0500

    Update StaticWebScraper.py

--- StaticWebScraper.py
+++ StaticWebScraper.py
@@ -1,9 +1,13 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
 
 
 headers = {'Accept-Encoding': 'identity'}
 url = 'https://61040-fa22.github.io/3-recitation-and-studio.html'
 r = requests.get(url, headers=headers)
 
-print(r.text)
+htmlMaterial = r.text
+
+soup = BeautifulSoup(htmlMaterial, 'html.parser')
+
+print(soup)
